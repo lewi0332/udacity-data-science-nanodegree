d9cf892545b6ed05469f4ba35874775e80f63c98
derricklewis <derrickjameslewis@gmail.com>
2021-04-05 08:17:46 -0400

    Package project framework

---
+++ software_engineering/PROJECT - Upload a Package tp PyPi/project_tbd/project_tbd.py
@@ -0,0 +1,4 @@
+class ProjectTBD:
+
+    def __init__(self, arg):
+        self.arg = arg
---
+++ software_engineering/PROJECT - Upload a Package tp PyPi/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='projectname',
+      version='0.1',
+      description='Project TBD',
+      packages=['ProjectName'],
+      author='Derrick Lewis',
+      zip_safe=False)
